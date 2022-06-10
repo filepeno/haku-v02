7aa67c55b907e09b9679ec0dcc7a4eaf373a5d72
filip <fso@stromlin.com>
2022-06-10 13:30:13 +0200

    fix customSite logic in search request module

--- js/search-request.js
+++ js/search-request.js
@@ -1,10 +1,10 @@
 export default async function searchRequest(customSite, raw) {
   const defaultSite = "knowit";
   let site = defaultSite;
-  if (customSite != null) {
+  if (customSite != undefined || customSite != null) {
     site = customSite;
   }
-  console.log("site", site);
+  console.log("customSite:", customSite);
   const url = `https://stromlin-es.test.headnet.dk/site-da-${site}/_search/template`;
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
