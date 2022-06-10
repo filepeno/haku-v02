9d8509f82b0e6c9d62960ca9049d38ffa60e5141
filip <fso@stromlin.com>
2022-06-10 10:03:16 +0200

    fix pagination: sen custom site as params

--- js/full-search.js
+++ js/full-search.js
@@ -25,7 +25,7 @@ export async function findAll(customSite, query, scope) {
       from: offset,
     },
   });
-  cleanResults(await searchRequest(customSite, raw));
+  cleanResults(await searchRequest(customSite, raw), customSite);
 }
 
 function calculateOffset() {
@@ -50,7 +50,7 @@ function calculateScope() {
   displayScope(from, to);
 }
 
-function cleanResults(result) {
+function cleanResults(result, customSite) {
   console.log("result", result);
   const hits = result.hits;
   const content = hits.hits;
@@ -62,7 +62,7 @@ function cleanResults(result) {
   //if first search
   if (currentScope === 1) {
     displayResultFeedback(q, totalHits);
-    initPagination(q, totalHits, size, maxPages);
+    initPagination(q, totalHits, size, maxPages, customSite);
   }
   calculateScope(offset, size);
 }
--- js/pagination.js
+++ js/pagination.js
@@ -2,7 +2,7 @@ import Pagination from "tui-pagination";
 import { clearResults } from "./search-interface";
 import { findAll } from "./full-search";
 
-export function initPagination(q, hits, size, pages) {
+export function initPagination(q, hits, size, pages, customSite) {
   const container = document.getElementById("tui-pagination-container");
   const pagination = new Pagination(container, {
     totalItems: hits,
@@ -22,6 +22,6 @@ export function initPagination(q, hits, size, pages) {
   pagination.on("afterMove", ({ page }) => {
     console.log("new page:", page);
     clearResults();
-    findAll(q, page);
+    findAll(customSite, q, page);
   });
 }
